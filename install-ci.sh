5a4cb818abed291525dd55fcfe972ebedc2e5aaa
Raphael Schumacher <raphael.schumacher@continuous.team>
2020-09-18 16:57:49 +0000

    fix ci

--- install-ci.sh
+++ install-ci.sh
@@ -3,7 +3,5 @@ echo "gem: --no-ri --no-rdoc" > ~/.gemrc
 \curl -sSL https://get.rvm.io | bash -s stable --rails
 source "$HOME/.rvm/scripts/rvm"
 bundle install
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt update && sudo apt install yarn
+curl -o- -L https://yarnpkg.com/install.sh | bash
 bundle exec rake db:migrate
