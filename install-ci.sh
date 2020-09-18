6a9bd0b2e7cf084e9441430bd3da74002414ef4b
Raphael Schumacher <raphael.schumacher@continuous.team>
2020-09-18 17:28:22 +0000

    fix ci

--- install-ci.sh
+++ install-ci.sh
@@ -4,5 +4,6 @@ echo "gem: --no-ri --no-rdoc" > ~/.gemrc
 source "$HOME/.rvm/scripts/rvm"
 bundle install
 curl -o- -L https://yarnpkg.com/install.sh | bash
+exec bash
 export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
 bundle exec rake db:migrate
